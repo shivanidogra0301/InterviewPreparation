3dc8ea5c2a2545ae48b4ee3fcd02bc5fbfba00ea
shivanidogra0301 <shivanodogra789@gmail.com>
2021-04-21 14:30:26 +0530

    New Files added

--- test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args){
-        int a = 100000;
-        int b = 100000;
-        long ans =(long) a * b;
-        System.out.println(ans);
-    }
-}
